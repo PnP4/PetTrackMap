b712b4e85a119a5abe1a5071d8711423483ace3f
knadikari <knadikari@gmail.com>
2016-09-18 22:48:53 +0530

    complete network call, add broadcast recever

--- app/src/main/java/com/ucsc/pnp/pettrack/MainView.java
+++ app/src/main/java/com/ucsc/pnp/pettrack/MainView.java
@@ -5,6 +5,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -23,21 +24,37 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainView extends AppCompatActivity {
     GoogleMap googleMap;
     Marker petpointer;
+    LocationbroadReceiver locationReceiver;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+Toast.makeText(getApplicationContext(),"Welcome",Toast.LENGTH_LONG).show();
+        IntentFilter filter = new IntentFilter("com.ucsc.pnp.pettrack.CUSTOM");
 
+        locationReceiver = new LocationbroadReceiver();
+        registerReceiver(locationReceiver, filter);
+
+        startService(new Intent(MainView.this,NetService.class));
         googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
 
-        showLocation("He he");
 
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(locationReceiver);
     }
 
     @Override
@@ -62,23 +79,30 @@ public class MainView extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void showLocation(String petname){
+    public void showLocation(String petname,LatLng newpsition){
 
-         LatLng newpsition = new LatLng(21 , 57);
+
         if(googleMap!=null) {
             petpointer = googleMap.addMarker(new MarkerOptions().position(newpsition).title(petname));
             googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(newpsition.latitude, newpsition.longitude), 12.0f));
         }
     }
-    public class LocationReceiver extends BroadcastReceiver {
+    public class LocationbroadReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Toast.makeText(context, "Intent Detected.", Toast.LENGTH_LONG).show();
-            showAlert("Pet NAme");
+
+            String msg=intent.getStringExtra("data");
+            try {
+                JSONObject obj = new JSONObject(msg);
+                showAlert(obj.getString("petname"),new LatLng(obj.getLong("lat"),obj.getLong("lon")));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 
-    public void showAlert(final String Petname){
+    public void showAlert(final String Petname,final LatLng loc){
         AlertDialog alertDialog = new AlertDialog.Builder(MainView.this).create();
         alertDialog.setTitle("Pet Alert");
         alertDialog.setMessage("Your pet is out of range :- PetName ::-- "+Petname);
@@ -86,7 +110,7 @@ public class MainView extends AppCompatActivity {
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
-                        showLocation(Petname);
+                        showLocation(Petname,loc);
                     }
                 });
         alertDialog.show();
